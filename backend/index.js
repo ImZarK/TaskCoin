b5f5fa41d00b5d6f59af79c4679f0bffed829115
ZarK <herrerakevin83@gmail.com>
2025-06-13 13:46:27 +0200

    cors 1.1

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(mongoSanitize());
 app.use(limiter);
 
--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,40 +1,30 @@
-
-
 import axios from 'axios';
 
-
 const apiClient = axios.create({
-    baseURL: 'http://localhost:5000/api', 
- 
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
 });
 
 
 apiClient.interceptors.request.use(
     (config) => {
-     
         const token = localStorage.getItem('token');
-        
-     
         if (token) {
             config.headers['Authorization'] = `Bearer ${token}`;
         }
         return config; 
     },
     (error) => {
-        
         return Promise.reject(error);
     }
 );
 
 apiClient.interceptors.response.use(
     (response) => {
-     
         return response;
     },
     (error) => {
-    
         if (error.response && error.response.status === 401) {
-     
+           // Aquí se podría añadir lógica para desloguear al usuario si un token expira
         }
         return Promise.reject(error);
     }
